72088eb7c69b3d9212df79eddb7a175d9dca9e2a
Martin <martin.matura123@gmail.com>
2021-11-22 16:04:12 +0100

    22.11.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriveMode.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Basic Drive Mode", group="Linear Opmode")
+public class BasicDriveMode extends LinearOpMode {
+    HardwareRobot robot = new HardwareRobot();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        robot.init(hardwareMap);
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()){
+            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_y;
+        }
+    }
+}
